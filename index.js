d0d85d243490e7a9d00a840103e560ffe17992ef
Waverly <Christiandramahadewa@gmail.com>
2025-05-27 13:21:19 +0845

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed("#typed-text", {
+      strings: [
+        "Siswa penuh semangat!",
+        "Siap menaklukkan dunia digital!",
+      ],
+      typeSpeed: 90,
+      backSpeed: 30,
+      loop: true,
+      backDelay: 1500,
+    });
+  });
